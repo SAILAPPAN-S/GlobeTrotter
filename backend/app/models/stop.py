56604a23ac0d22b8af1f15ede3e830de08ba15d3
SAILAPPAN S <sailappan9790@gmail.com>
2026-01-03 16:57:22 +0530

    Delete backend directory

--- backend/app/models/stop.py
+++
@@ -1,9 +0,0 @@
-from app.extensions import db
-
-class TripStop(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    trip_id = db.Column(db.Integer, db.ForeignKey("trip.id"))
-    city = db.Column(db.String(100))
-    country = db.Column(db.String(100))
-    start_date = db.Column(db.Date)
-    end_date = db.Column(db.Date)
